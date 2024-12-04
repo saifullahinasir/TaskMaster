d28468bc0527f4f1f492c1f0c97790334bff7ed0
saifullahinasir <saifullahin677@gmail.com>
2024-12-04 01:49:47 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.mongodb+srv, {
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
