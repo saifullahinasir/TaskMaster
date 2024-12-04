a9a7285ea45a5e6eaa738b3b9ca9189b8a1a013c
saifullahinasir <saifullahin677@gmail.com>
2024-12-04 01:51:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // MongoDB Connection
-mongoose.connect(process.env.mongodb+srv)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
